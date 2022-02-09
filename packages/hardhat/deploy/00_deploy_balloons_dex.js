2a47838dbfc8aa8df22af6b6942ded3d593df4e4
yehorrudytsia <yehorrudytsia@gmail.com>
2022-02-09 08:55:07 +0200

    Init dex on deploy

--- packages/hardhat/deploy/00_deploy_balloons_dex.js
+++ packages/hardhat/deploy/00_deploy_balloons_dex.js
@@ -18,16 +18,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log: true,
   });
 
-  // const dex = await ethers.getContract("DEX", deployer);
+  const dex = await ethers.getContract("DEX", deployer);
 
 
   await balloons.transfer("0x744803E078BaF3d3209c71a7876e53b9c55E6949",""+(10*10**18));
 
-  // uncomment to init DEX on deploy:
-  // console.log("Approving DEX ("+dex.address+") to take Balloons from main account...")
-  // If you are going to the testnet make sure your deployer account has enough ETH
-  // await balloons.approve(dex.address,ethers.utils.parseEther('100'));
-  // console.log("INIT exchange...")
-  // await dex.init(""+(3*10**18),{value:ethers.utils.parseEther('3'),gasLimit:200000})
+  console.log("Approving DEX ("+dex.address+") to take Balloons from main account...")
+  await balloons.approve(dex.address,ethers.utils.parseEther('100'));
+  console.log("INIT exchange...")
+  await dex.init(""+(3*10**18),{value:ethers.utils.parseEther('3'),gasLimit:200000})
 };
 module.exports.tags = ["Balloons", "DEX"];
